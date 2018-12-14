f66198b4f8aceca5308225da6f3f8fb3814156cd
TheOrzo <tobihogh@gmail.com>
2018-12-14 22:44:42 +0100

    Added "add" command to CLI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        new UserInterface();
+        SortedIntList list = new SortedIntList();
+
+        new UserInterface(list);
     }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,9 +6,9 @@ public class UserInterface {
 
     private SortedIntList list;
 
-    public UserInterface() {
+    public UserInterface(SortedIntList list) {
         exit = false;
-        list = new SortedIntList();
+        this.list = list;
 
         inputLoop();
     }
@@ -18,11 +18,25 @@ public class UserInterface {
             Terminal.printLine("> ");
 
             String line = Terminal.readLine();
-            switch(line) {
+            switch(line.split(" ")[0]) {
                 case "quit":
                     Terminal.printLine("Bye");
                     exit = true;
                     break;
+                case "add":
+                    String[] cmd = line.split(" ");
+                    if (cmd.length != 2) {
+                        Terminal.printLine("Wrong amount of arguments. Usage: add [number to be added]");
+                        return;
+                    }
+
+                    if (cmd[1].matches("^-?[0-9]*$")) {
+                        list.add(Integer.parseInt(cmd[1]));
+                    } else {
+                        Terminal.printLine(cmd[1] + " is supposed to be a number");
+                    }
+                    break;
+
                 case "print":
                     Terminal.printLine(list.print());
                     break;
